056087eba7d44c2dbd7f2eba00e409cb8262f023
nucccio <ca591sav@htwg-konstanz.de>
2025-11-26 11:40:12 +0100

    Iteration 2 - category.html into App.vue

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-const repoName = '/frontend-casellese';  // Stimmt das ->  / ?
+const repoName = 'frontend-casellese';  // Stimmt das ->  / ?
 
 // https://vite.dev/config/
 export default defineConfig({
