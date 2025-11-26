13a1717b60ec2f78d9278a2068337f2a75f51176
nucccio <ca591sav@htwg-konstanz.de>
2025-11-26 13:53:13 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-const repoName = 'frontend-casellese';  // Stimmt das ->  / ?
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? repoName : '/',
   plugins: [
     vue(),
     vueDevTools(),
